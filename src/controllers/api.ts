93095e608664e10ed111d85112284a93b5eb9f0c
WilmerRS <estwilmerrodriguez@gmail.com>
2022-06-06 12:00:16 -0500

    fix: remove dist/src

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -2,6 +2,7 @@ import { Application, Request, Response } from "express";
 
 export const loadApiEndpoints = (app: Application): void => {
   app.get("/api", (req: Request, res: Response) => {
+    console.log("API endpoint hit at", new Date().toString());
     return res.status(200).send({ status: "ok" });
   });
 };
